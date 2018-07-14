6e1f59c88c6e13aaff1f6f0ff60a98c8026b41d4
Celso Oliveira <celsooliveira@Celsos-MacBook-Air.local>
2018-07-14 11:22:28 -0700

    add jawssql

--- server.js
+++ server.js
@@ -5,15 +5,6 @@ const methodOverride = require('method-override');
 const passport 			 = require("./config/passport");
 const config				 = require("./config/extra-config");
 const session        = require('express-session'); 
-
-const DATABASE_URL = "postgres://oiakkjytnuroxm:97395421edfcd266fce4d29981527d9aed112509375ec44535b1823bb2262432@ec2-54-83-33-213.compute-1.amazonaws.com:5432/dclmb16fqthekg"
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-
-
 const app            = express();
 const PORT           = process.env.PORT || 3000;
 const db             = require('./models');
@@ -32,11 +23,9 @@ var isAuth 				 = require("./config/middleware/isAuthenticated");
 var authCheck 		 = require('./config/middleware/attachAuthenticationStatus');
 
 app.use(methodOverride('_method'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); 
-
 app.use(session({ secret: config.sessionKey, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
